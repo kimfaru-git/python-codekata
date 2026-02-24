2c29fde84506b850139f476c64b16834efe45ed1
kimfaru-git <khu3296@gmail.com>
2026-02-24 16:12:25 +0900

    [프로그래머스] L0 - 점의 위치 구하기 (#23)

---
+++ programmers/L0_기초입문/입문/023_점의_위치_구하기.py
@@ -0,0 +1,17 @@
+# 점의 위치 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120841
+# 알고리즘: 기초
+# 작성자: 김형욱
+# 작성일: 2026. 02. 24. 16:12:24
+
+def solution(dot):
+    if dot[0] > 0 and dot[1] > 0:
+        answer = 1
+    elif dot[0] < 0 and dot[1] > 0:
+        answer = 2
+    elif dot[0] < 0 and dot[1] < 0:
+        answer = 3
+    elif dot[0] > 0 and dot[1] < 0:
+        answer = 4
+    return answer
