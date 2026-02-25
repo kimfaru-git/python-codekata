c55fdd9d9f582ffc655eab57f2adfa0a00cb3d9b
kimfaru-git <khu3296@gmail.com>
2026-02-25 14:37:42 +0900

    [프로그래머스] L0 - 배열의 유사도 (#24)

--- programmers/L0_기초입문/입문/024_배열의_유사도.py
+++ programmers/L0_기초입문/입문/024_배열의_유사도.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120903
 # 알고리즘: 기초
 # 작성자: 김형욱
-# 작성일: 2026. 02. 25. 14:25:48
+# 작성일: 2026. 02. 25. 14:37:42
 
 def solution(s1, s2):
     answer = 0
